8ddd439bd34ac26e873e51870e51f67dcb47b1a1
sungwonkim-muhayu <sungwonkim@muhayu.com>
2022-10-14 23:42:28 +0900

    [feat] QueryDsl을 활용한 Custom Repository 추가

--- src/main/java/com/sungwonkim/querydsltutorial/repository/BlogRepository.java
+++ src/main/java/com/sungwonkim/querydsltutorial/repository/BlogRepository.java
@@ -1,8 +1,9 @@
 package com.sungwonkim.querydsltutorial.repository;
 
 import com.sungwonkim.querydsltutorial.model.entity.Blog;
+import com.sungwonkim.querydsltutorial.repository.custom.QueryDslBlogRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface BlogRepository
-    extends JpaRepository<Blog, Long>, QuerydslPredicateExecutor<Blog> {}
+    extends JpaRepository<Blog, Long>, QuerydslPredicateExecutor<Blog>, QueryDslBlogRepository {}
